6ba381aee19ebf37e2eed417fc8aa073bbbb7a97
LMoon <jjay033033@gmail.com>
2018-04-12 10:03:41 +0800

    test

---
+++ src/main/java/top/lmoon/servlet/TestServlet.java
@@ -0,0 +1,31 @@
+package top.lmoon.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1128635005944926844L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		writer.print("hello test!");
+		writer.close();
+	}
+
+}
