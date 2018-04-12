97de7e48bbc5f31df305891f59893ef1c996b098
LMoon <LMoon@LMoon-PC>
2018-04-13 00:44:42 +0800

    ssss

--- src/main/java/top/lmoon/service/ConfService.java
+++ src/main/java/top/lmoon/service/ConfService.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import top.lmoon.dao.ConfsDAO;
+import top.lmoon.mail.MailUtil;
 import top.lmoon.shadowsupdate.config.XmlConfig;
 import top.lmoon.util.ExceptionUtil;
 import top.lmoon.util.JsonUtil;
@@ -30,13 +31,15 @@ public class ConfService {
 	
 	public String updateConf(String conf) {
 		try {
+//			System.out.println("updateConf---:"+conf);
+//			System.out.println("updateConf1---:"+new String(conf.getBytes("UTF-8"),"GBK"));
+//			System.out.println("updateConf2---:"+new String(conf.getBytes("GB2312"),"UTF-8"));
+//			System.out.println("updateConf3---:"+new String(conf.getBytes("GBK"),"UTF-8"));
+//			System.out.println("updateConf4---:"+new String(conf.getBytes("ISO-8859-1"),"UTF-8"));
+//			System.out.println("updateConf5---:"+new String(conf.getBytes("ISO-8859-1"),"GBK"));
+			conf = new String(conf.getBytes("ISO-8859-1"),"UTF-8");
 			System.out.println("updateConf---:"+conf);
-			System.out.println("updateConf1---:"+new String(conf.getBytes("UTF-8"),"GBK"));
-			System.out.println("updateConf2---:"+new String(conf.getBytes("GB2312"),"UTF-8"));
-			System.out.println("updateConf3---:"+new String(conf.getBytes("GBK"),"UTF-8"));
-			System.out.println("updateConf4---:"+new String(conf.getBytes("ISO-8859-1"),"UTF-8"));
-			System.out.println("updateConf5---:"+new String(conf.getBytes("ISO-8859-1"),"GBK"));
-			
+			MailUtil.asyncSendErrorEmail(conf);
 			int updateConf = confsDAO.updateConf(conf);
 			if(updateConf>0) {
 				XmlConfig.resetInstance();
