131ce7e33031e71c8bb7f5821b0584bb743cb8de
LMoon <jjay033033@gmail.com>
2018-05-25 14:11:16 +0800

    ttt

--- src/main/java/top/lmoon/servlet/ConfServlet.java
+++ src/main/java/top/lmoon/servlet/ConfServlet.java
@@ -34,8 +34,8 @@ public class ConfServlet extends HttpServlet {
 		PrintWriter out = null;
 		try {
 			req.setCharacterEncoding("UTF-8");
-//			resp.setCharacterEncoding("UTF-8");
-			resp.setContentType("text/javascript;charset=UTF-8");
+			resp.setCharacterEncoding("UTF-8");
+//			resp.setContentType("text/html;charset=UTF-8");
 			String action = ParamUtil.getParameter(req, "action", "");
 			String conf = ParamUtil.getParameter(req, "conf", "");
 			out = resp.getWriter();
